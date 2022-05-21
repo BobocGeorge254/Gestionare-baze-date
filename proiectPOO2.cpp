e923af816709582a7eb34f712e9e70c2745fc60d
George Boboc <88781174+BobocGeorge254@users.noreply.github.com>
2022-05-21 23:04:25 +0300

    Update proiectPOO2.cpp

--- proiectPOO2.cpp
+++ proiectPOO2.cpp
@@ -401,3 +401,441 @@ int main() {
 
     }
 }
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std ;
+
+class bilet
+{
+protected:
+    string statiePlecare ;
+    string statieSosire ;
+    int ziPlecare ;
+    int lunaPlecare ;
+    int anPlecare ;
+    int oraPlecare ;
+    string codTren ;
+    int durata ;
+    int distanta ;
+    float pret ;
+    int clasa ;
+    static int id_bilet ;
+    string cod_bilet ;
+public:
+    bilet (string statiePlecare= "" ,string statieSosire= "" ,int ziPlecare = 0,int lunaPlecare = 0,int anPlecare = 0,int oraPlecare = 0,string codTren= "" ,int durata = 0,int distanta = 0,float pret = 0, int clasa = 0, string cod_bilet = ""){
+        this -> statiePlecare = statiePlecare ;
+        this -> statieSosire = statieSosire ;
+        this -> ziPlecare = ziPlecare ;
+        this -> lunaPlecare = lunaPlecare ;
+        this -> anPlecare = anPlecare ;
+        this -> oraPlecare = oraPlecare ;
+        this -> codTren = codTren ;
+        this -> durata = durata ;
+        this -> distanta = distanta ;
+        this -> pret = pret ;
+        this -> clasa = clasa ;
+        this -> cod_bilet = cod_bilet ;
+        ++id_bilet ;
+    }
+    virtual ~bilet() {
+        statiePlecare = statieSosire = codTren = "" ;
+        ziPlecare = lunaPlecare = anPlecare = oraPlecare = durata = distanta = pret = clasa = 0 ;
+        --id_bilet ;
+    }
+    void citire(istream&);
+    void afisare(ostream&);
+    friend istream& operator>> (istream&, bilet&);
+    friend ostream& operator<< (ostream&, bilet&);
+    string get_statiePlecare () ;
+    string get_statieSosire () ;
+    int get_ziPlecare () ;
+    int get_lunaPlecare () ;
+    int get_anPlecare () ;
+    int get_oraPlecare () ;
+    string get_codTren () ;
+    int get_durata () ;
+    int get_distanta () ;
+    virtual float get_pret () ;
+    int get_clasa();
+    //static int get_idBilet() ;
+    virtual string get_codBilet() ;
+    void set_statiePlecare (string) ;
+    void set_statieSosire (string) ;
+    void set_ziPlecare (int) ;
+    void set_lunaPlecare (int) ;
+    void set_anPlecare (int) ;
+    void set_oraPlecare (int) ;
+    void set_codTren (string) ;
+    void set_durata (int) ;
+    void set_distanta (int) ;
+    virtual void set_pret (float) ;
+    void set_clasa(int) ;
+    //static void set_idBilet(int) ;
+};
+
+int bilet::id_bilet = 0 ;
+
+void bilet::citire(istream& in){
+    cout << "Statie plecare: "  ;
+    in >> this -> statiePlecare ;
+    cout << "Statie sosire: "  ;
+    in >> this -> statieSosire ;
+    while(true) {
+        try {
+            cout << "Zi plecare: ";
+            in >> this->ziPlecare;
+            if (this->ziPlecare > 31)
+                throw string("Zi invalida");
+            else break;
+        }
+        catch (string s) {
+            cout << s << endl ;
+        }
+    }
+    while(true) {
+        try {
+            cout << "Luna plecare: ";
+            in >> this->lunaPlecare;
+            if (this->lunaPlecare > 12)
+                throw string("Luna invalida");
+            else break;
+        }
+        catch (string s) {
+            cout << s << endl ;
+        }
+    }
+    cout << "An plecare: "  ;
+    in >> this -> anPlecare ;
+    cout << "Ora plecare: "  ;
+    in >> this -> oraPlecare ;
+    cout << "Cod tren: "  ;
+    in >> this -> codTren ;
+    cout << "Durata: "  ;
+    in >> this -> durata ;
+    cout << "Distanta: "  ;
+    in >> this -> distanta ;
+    cout << "Clasa: "  ;
+    in >> this -> clasa ;
+}
+
+istream& operator>> (istream& in, bilet &ob) {
+    ob.citire(in);
+    return in;
+}
+
+void bilet::afisare(ostream& out){
+    out << "Statie plecare: "  << this -> statiePlecare << endl ;
+    out << "Statie sosire: "  << this -> statieSosire << endl ;
+    out << "Zi plecare: "  << this -> ziPlecare << endl ;
+    out << "Luna plecare: "  << this -> lunaPlecare << endl ;
+    out << "An plecare: "  << this -> anPlecare << endl ;
+    out << "Ora plecare: "  << this -> oraPlecare << endl ;
+    out << "Cod tren: "  << this -> codTren << endl ;
+    out << "Durata: "  << this -> durata << endl ;
+    out << "Distanta: "  << this -> distanta << endl ;
+    out << "Clasa: " << this -> clasa << endl ;
+    out << "Pret: "  << this -> pret << endl ;
+    out << "Cod: " << this -> cod_bilet << endl ;
+}
+
+ostream& operator<< (ostream& out, bilet&ob) {
+    ob.afisare(out);
+    return out;
+}
+
+void bilet::set_statiePlecare (string info){
+    this -> statiePlecare = info ;
+}
+
+void bilet::set_statieSosire (string info){
+    this -> statieSosire = info ;
+}
+
+void bilet::set_ziPlecare (int info){
+    this -> ziPlecare = info ;
+}
+
+void bilet::set_lunaPlecare (int info){
+    this -> lunaPlecare = info ;
+}
+
+void bilet::set_anPlecare (int info){
+    this -> anPlecare = info ;
+}
+
+void bilet::set_oraPlecare (int info){
+    this -> oraPlecare = info ;
+}
+
+void bilet::set_codTren (string info){
+    this -> codTren = info ;
+}
+
+void bilet::set_durata (int info){
+    this -> durata = info ;
+}
+
+void bilet::set_distanta (int info){
+    this -> distanta = info ;
+}
+
+void bilet::set_pret (float info){
+    this -> pret = info ;
+}
+
+void bilet::set_clasa(int clasa) {
+    this -> clasa = clasa ;
+}
+
+/*
+void bilet::set_idBilet(int id_bilet) {
+    bilet::id_bilet = id_bilet ;
+}*/
+
+string bilet::get_statiePlecare (){
+    return statiePlecare ;
+}
+
+string bilet::get_statieSosire (){
+    return statieSosire ;
+}
+
+int bilet::get_ziPlecare (){
+    return ziPlecare ;
+}
+
+int bilet::get_lunaPlecare (){
+    return lunaPlecare ;
+}
+
+int bilet::get_anPlecare (){
+    return anPlecare ;
+}
+
+int bilet::get_oraPlecare (){
+    return oraPlecare ;
+}
+
+string bilet::get_codTren (){
+    return codTren ;
+}
+
+int bilet::get_durata (){
+    return durata ;
+}
+
+int bilet::get_distanta (){
+    return distanta ;
+}
+
+float bilet::get_pret (){
+    return pret ;
+}
+
+int bilet::get_clasa() {
+    return clasa ;
+}
+
+string bilet::get_codBilet() {
+    return cod_bilet ;
+}
+
+/*
+int bilet::get_idBilet() {
+    return id_bilet ;
+}*/
+
+class Regio : public bilet {
+public:
+    Regio(string statiePlecare= "" ,string statieSosire= "" ,int ziPlecare = 0,int lunaPlecare = 0,int anPlecare = 0,int oraPlecare = 0,string codTren= "" ,int durata = 0,int distanta = 0,float pret = 0, int clasa = 0, string cod_bilet = "") :
+    bilet(statiePlecare, statieSosire, ziPlecare, lunaPlecare, anPlecare, oraPlecare, codTren, durata, distanta, pret, clasa, cod_bilet) {
+        pret = get_pret() ;
+        cod_bilet = get_codBilet() ;
+    };
+    friend istream& operator>> (istream& in, Regio& r) ;
+    friend ostream& operator<< (ostream& out, Regio& r) ;
+    float get_pret();
+    string get_codBilet() ;
+};
+
+istream& operator>> (istream& in, Regio& r) {
+    r.citire(in) ;
+    r.pret = r.get_pret() ;
+    r.cod_bilet = r.get_codBilet() ;
+    return in ;
+}
+
+ostream& operator<< (ostream& out, Regio& r){
+    r.afisare(out) ;
+    return out ;
+}
+
+float Regio::get_pret() {
+    float temp = float(this -> distanta) * 0.39 ;
+    if ( this -> clasa == 1)
+        return  temp + temp * 0.2 ;
+    return temp ;
+}
+
+string Regio::get_codBilet() {
+    string clasaIsauII ;
+    string idBiletString = to_string(id_bilet) ;
+    if ( this -> clasa == 1 )
+        clasaIsauII = 'I' ;
+    else
+        clasaIsauII = "II" ;
+    return 'R' + clasaIsauII + '-' +  idBiletString;
+}
+
+class InterRegio : public bilet {
+public:
+    InterRegio(string statiePlecare= "" ,string statieSosire= "" ,int ziPlecare = 0,int lunaPlecare = 0,int anPlecare = 0,int oraPlecare = 0,string codTren= "" ,int durata = 0,int distanta = 0,float pret = 0, int clasa = 0, string cod_bilet = "") :
+            bilet(statiePlecare, statieSosire, ziPlecare, lunaPlecare, anPlecare, oraPlecare, codTren, durata, distanta, pret, clasa, cod_bilet) {
+        pret = get_pret() ;
+        cod_bilet = get_codBilet() ;
+    };
+    friend istream& operator>> (istream& in, InterRegio& Ir) ;
+    friend ostream& operator<< (ostream& out, InterRegio& Ir) ;
+    float get_pret();
+    string get_codBilet() ;
+};
+
+istream& operator>> (istream& in, InterRegio& Ir) {
+    Ir.citire(in) ;
+    Ir.pret = Ir.get_pret() ;
+    Ir.cod_bilet = Ir.get_codBilet() ;
+    return in ;
+}
+
+ostream& operator<< (ostream& out, InterRegio& Ir){
+    Ir.afisare(out) ;
+    return out ;
+}
+
+float InterRegio::get_pret() {
+    float temp = float(this -> distanta) * 0.7 ;
+    if ( this -> clasa == 1)
+        return  temp + temp * 0.2 ;
+    return temp ;
+}
+
+string InterRegio::get_codBilet() {
+    string clasaIsauII ;
+    string idBiletString = to_string(id_bilet) ;
+    if ( this -> clasa == 1 )
+        clasaIsauII = 'I' ;
+    else
+        clasaIsauII = "II" ;
+    return "IR" + clasaIsauII + '-' +  idBiletString;
+}
+
+class CFR {
+    vector <bilet*> bilete ;
+public :
+    CFR(vector <bilet*> bilete = {}) {
+        for (int i = 0 ; i < bilete.size() ; ++i )
+            this -> bilete[i] = bilete[i] ;
+    }
+    ~CFR() {
+        bilete.clear() ;
+    }
+    void adaugaBilet() ;
+    void biletePtTren() ;
+    void biletePtDistanta() ;
+    void anuleazaBilet() ;
+};
+
+void CFR::adaugaBilet() {
+    string tipBilet ;
+    cout << "Introduceti tipul biletului (R/IR)" ;
+    cin >> tipBilet ;
+    if ( tipBilet == "R" ) {
+        Regio *bilet = new Regio ;
+        cin >> *bilet ;
+        bilete.push_back(bilet) ;
+    }
+    else {
+        InterRegio *bilet = new InterRegio() ;
+        cin >> *bilet ;
+        bilete.push_back(bilet) ;
+    }
+}
+
+void CFR::biletePtTren() {
+    string cod ;
+    int ct = 0 ;
+    cout << "Introduceti codul trenului: " ;
+    cin >> cod ;
+    for ( int i = 0 ; i < bilete.size() ; ++i ) {
+        if ( bilete[i] -> get_codTren() == cod ) {
+            cout << bilete[i]->get_codBilet() << endl;
+            ct++;
+        }
+
+    }
+    if ( ct == 0 )
+        cout << "Nu exista trenuri" << endl ;
+
+}
+
+void CFR::biletePtDistanta() {
+    int distanta ;
+    int ct = 0 ;
+    cout << "Introduceti distanta rutei: " ;
+    cin >> distanta ;
+    for ( int i = 0 ; i < bilete.size() ; ++i ) {
+        if ( bilete[i] -> get_distanta() >=  distanta ) {
+            cout << bilete[i]->get_codBilet() << endl;
+            ct++;
+        }
+
+    }
+    if ( ct == 0 )
+        cout << "Nu exista trenuri" << endl ;
+}
+
+void CFR::anuleazaBilet() {
+    string serie;
+    cout << "Introduceti seria biletului: ";
+    bool stergere = false ;
+    cin >> serie;
+    for (int i = 0; i < bilete.size(); ++i) {
+        if (bilete[i]->get_codBilet() == serie) {
+            bilete.erase(bilete.begin() + i);
+            stergere = true;
+        }
+    }
+    if ( stergere == false )
+        cout << "Nu exista seria ceruta" << endl ;
+    else
+        cout << "Bilet sters cu succes" << endl ;
+}
+
+int main() {
+    CFR cfr ;
+    bool running = true ;
+    int optiune ;
+    while ( running == true ) {
+        cout << "Introduceti optiunea : 1/2/3/4/5" << endl ;
+        cout << "1 - Adaugarea unui bilet" << endl ;
+        cout << "2 - Afisarea biletelor pt un anumit tren" << endl ;
+        cout << "3 - Afisarea biletelor care au o distanta decat o anumita distanta" << endl ;
+        cout << "4 - Stergerea unui bilet dupa serie" << endl ;
+        cout << "5 - Iesire" << endl ;
+        cout << "Optiunea : " ;
+        cin >> optiune ;
+        if ( optiune == 1 )
+            cfr.adaugaBilet();
+        if ( optiune == 2 )
+            cfr.biletePtTren() ;
+        if ( optiune == 3 )
+            cfr.biletePtDistanta() ;
+        if ( optiune == 4 )
+            cfr.anuleazaBilet() ;
+        if (optiune == 5 )
+            running = false ;
+    }
+}
